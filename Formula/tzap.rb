9f75be5dd1623c55ffc90778707d9a121f9a06dc
frankzhu <frankmanzhu@hotmail.com>
2026-07-01 18:20:45 +1000

    release: update Homebrew formula for 0.1.9

--- Formula/tzap.rb
+++ Formula/tzap.rb
@@ -1,26 +1,26 @@
 class Tzap < Formula
   desc "Create, list, verify, and extract encrypted recoverable tzap archives"
   homepage "https://github.com/tzap-org/tzap"
-  version "0.1.8"
+  version "0.1.9"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/tzap-org/tzap/releases/download/v#{version}/tzap-v#{version}-macos-aarch64.tar.gz"
-      sha256 "6f36718b6b2dbce82831133dab6e1b93d5595725441a508c9a48b82241ecd4dc"
+      sha256 "b5fa4ac1bfe0bee180ef4059da6b17211e324d23c09b95d15c27b3d82e8b62e3"
     else
       url "https://github.com/tzap-org/tzap/releases/download/v#{version}/tzap-v#{version}-macos-x86_64.tar.gz"
-      sha256 "155382431367c99d2b43b041e464f7aed009d0de32d939a34f4e25752c74d9de"
+      sha256 "f7677ea14c2f9d4f969d8a8e7c5c198cfb10c0b37caf829d7cb7d3ce51ef0ae1"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/tzap-org/tzap/releases/download/v#{version}/tzap-v#{version}-linux-x86_64-musl.tar.gz"
-      sha256 "d8b8119e6d21bdf9a10e843cedf5fdd893a896aee124e515abfbfbb43912a89a"
+      sha256 "164dd16de206773cc1e17cdad696a4d11f127da6c4159d9e6227a2d00b10ead5"
     else
       url "https://github.com/tzap-org/tzap/releases/download/v#{version}/tzap-v#{version}-linux-aarch64-musl.tar.gz"
-      sha256 "856302c5016719fd9c3f3f243841bd97f0ae34315fa4a525270e515a80a9ff0f"
+      sha256 "0d2e01ae39b8d1f03c4efb27845d02d391ecdab89d851906f1326cd6919f9346"
     end
   end
 
